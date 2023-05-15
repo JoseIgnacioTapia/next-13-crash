eab847de147e49f0bab55c5bc95c995226ded2e1
JoeIt <josignaciot@gmail.com>
2023-05-15 11:31:26 -0300

    Repos Page Output

--- app/code/repos/page.tsx
+++ app/code/repos/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa';
 import { Repository } from '@/APITypes/types';
 
 async function fetchRepos(): Promise<Repository[]> {
@@ -11,9 +13,33 @@ async function fetchRepos(): Promise<Repository[]> {
 
 async function ReposPage(): Promise<JSX.Element> {
   const repos = await fetchRepos();
-  console.log(repos);
 
-  return <div>{repos[0].name}</div>;
+  return (
+    <div className="repos-container">
+      <h2>Repositorios</h2>
+      <ul className="repo-list">
+        {repos.map((repo: Repository) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className="repo-details">
+                <span>
+                  <FaStar /> {repo.stargazers_count}
+                </span>
+                <span>
+                  <FaCodeBranch /> {repo.forks_count}
+                </span>
+                <span>
+                  <FaEye /> {repo.watchers_count}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ReposPage;

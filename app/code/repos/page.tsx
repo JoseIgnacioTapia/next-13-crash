cf7c94b5746d85056124502ea8a6b93ee53f3fcf
JoeIt <josignaciot@gmail.com>
2023-06-02 14:32:32 -0300

    Serving courses data

--- app/code/repos/page.tsx
+++ app/code/repos/page.tsx
@@ -4,7 +4,12 @@ import { Repository } from '@/APITypes/types';
 
 async function fetchRepos(): Promise<Repository[]> {
   const response = await fetch(
-    'https://api.github.com/users/bradtraversy/repos'
+    'https://api.github.com/users/bradtraversy/repos',
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
   );
 
   await new Promise(resolve => setTimeout(resolve, 1000)); // Wait 1 second
--- app/components/RepoDirs.tsx
+++ app/components/RepoDirs.tsx
@@ -7,7 +7,12 @@ async function fetchRepoContents(name: string): Promise<RootObject[]> {
   await new Promise(resolve => setTimeout(resolve, 3000));
 
   const response = await fetch(
-    `https://api.github.com/repos/bradtraversy/${name}/contents`
+    `https://api.github.com/repos/bradtraversy/${name}/contents`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
   );
   const contents = await response.json();
   return contents;
